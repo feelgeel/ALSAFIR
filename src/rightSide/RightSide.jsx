53132e4f2ea5bfa9becb84d570c849eef53a5dc1
cisco-laptop <feelgeel@gmail.com>
2023-05-23 15:34:47 +0100

    finishing shopping list

--- src/rightSide/RightSide.jsx
+++ src/rightSide/RightSide.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import prodImg from "../img/hero-small.jpg"
 
 const RightSide = () => {
-    let products={
-
-    }
+  let totalPrice=0
+    let products=[
+      {
+      prodImg,
+      prodName:"mounice",
+      prodQuant:1,
+      prodPricePerUnit:30,
+    },
+      {
+        prodImg,
+        prodName:"zitoun",
+        prodQuant:1,
+        prodPricePerUnit:30,
+    },
+  ]
   return (
     <div className="rightside">
       <table style={{width:"100%"}}>
@@ -16,23 +28,24 @@ const RightSide = () => {
           <th>price per quant</th>
           <th>product price</th>
         </tr>
-        <tr>
-          <td><img width="100px" height="50px" src={prodImg}/></td>
-          <td>mounise</td>
-          <td>5</td>
-          <td>30 DA</td>
-          <td>150 DA</td>
+        {
+          products.map(dt=>{
+            totalPrice+=dt.prodPricePerUnit*dt.prodQuant
+            
+            return <tr>
+          <td><img width="100px" height="50px" src={dt.prodImg}/></td>
+          <td>{dt.prodName}</td>
+          <td>{dt.prodQuant}</td>
+          <td>{dt.prodPricePerUnit}</td>
+          <td>{dt.prodPricePerUnit*dt.prodQuant}</td>
         </tr>
-        <tr>
-        <td>mounise</td>
-        <td>mounise</td>
-          <td>5</td>
-          <td>30 DA</td>
-          <td>150 DA</td>
-        </tr>
+          }
+          )
+        }
       </table>
       <div className="totalPrice">
-        <span>TOTAL PRICE 500 DA</span>
+        <span>TOTAL PRICE {totalPrice} DA</span>
+        <button>sold</button>
       </div>
     </div>
   );
